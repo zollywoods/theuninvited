79c2a0d17d2cdb04fe1b38ff343714e2cbd52c12
zollywoods <zooollywood@gmail.com>
2024-10-28 16:26:23 -0400

    second commit

--- pages/index.js
+++ pages/index.js
@@ -10,122 +10,70 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.homePage}>
+          <div className={styles.title}>
+            the uninvited
+          </div>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}> 
+             the film
+            </li>
+            <li className={styles.menuItem} id={styles.menuItemTwo}> 
+             the filmmakers
+            </li>
+            <li className={styles.menuItem} id={styles.menuItemThree}> 
+             the cast
+            </li>
+            <li className={styles.menuItem} id={styles.menuItemFour}> 
+             press
+            </li>
+            <li className={styles.menuItem} id={styles.menuItemFive}> 
+             audience reaction
+            </li>
+            <li className={styles.menuItem} id={styles.menuItemSix}> 
+             trailer and teasers gallery
+            </li>
+          </ul>
+          <img src="/imgs/splash.png" alt="poster" className={styles.splashImage} />
+          <div className={styles.names}>
+            <div className={styles.name}>
+            elizabeth <br/>
+            reaser
+            </div>
+            <div className={styles.name}>
+            walton <br/>
+            goggins
+            </div>
+            <div className={styles.name}>
+            lois <br/>
+            smith
+            </div>
+            <div className={styles.name}>
+            pedro <br/>
+            pascal
+            </div>
+            <div className={styles.name}>
+            rufus <br/>
+            sewell
+            </div>
+            <div className={styles.name}>
+            eva de <br/>
+            dominici
+            </div>
+          </div>
+          <div className={styles.actorImages}>  
+            <img src="/imgs/actors/1_actor.png" className={styles.actorImage} alt="elizabeth" />
+            <img src="/imgs/actors/2_actor.png" className={styles.actorImage} alt="walton" />
+            <img src="/imgs/actors/3_actor.png" className={styles.actorImage} alt="lois" />
+            <img src="/imgs/actors/4_actor.png" className={styles.actorImage} alt="pedro" />
+            <img src="/imgs/actors/5_actor.png" className={styles.actorImage} alt="rufus" />
+            <img src="/imgs/actors/6_actor.png" className={styles.actorImage} alt="eva" />
+          </div>
+          <div className={styles.textBox}>
+            
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
